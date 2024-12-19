154597b9ae603f1dedc1a96bc5657d020955d75e
Murali <muralisudireddy0@gmail.com>
2024-12-20 01:56:35 +0530

    updated mail service

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,28 +1,32 @@
 const nodemailer = require("nodemailer");
 const { AUTH_EMAIL, AUTH_PASS } = process.env;
+
 let transporter = nodemailer.createTransport({
-  host: "smtp-mail.outlook.com",
-  // port:587,
-  // secure:false,
+  service: "gmail",
   auth: {
     user: AUTH_EMAIL,
-    pass: AUTH_PASS,
+    pass: AUTH_PASS, // Use the App Password here
   },
 });
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.log(error);
-  }
-});
+if (process.env.NODE_ENV !== "production") {
+  transporter.verify((error, success) => {
+    if (error) {
+      console.error("Transporter verification failed:", error);
+    } else {
+      console.log("Transporter is ready for use:", success);
+    }
+  });
+}
 
 const sendEmail = async (mailOptions) => {
   try {
-    await transporter.sendMail(mailOptions)
-    console.log("Email sent");
+    await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully!");
   } catch (error) {
+    console.error("Error sending email:", error);
     throw error;
   }
 };
 
-module.exports = sendEmail;
+module.exports = {sendEmail}
